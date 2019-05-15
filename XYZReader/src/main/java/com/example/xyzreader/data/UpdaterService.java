c70e3ac48a44df9d34d5837c34c4c7fcd7197ce4
Brock <bulltex95@gmail.com>
2019-05-14 17:31:05 -0700

    Cleaned up code

--- XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
@@ -20,6 +20,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+@SuppressWarnings("deprecation")
 public class UpdaterService extends IntentService {
     private static final String TAG = "UpdaterService";
 
@@ -47,7 +48,7 @@ public class UpdaterService extends IntentService {
                 new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
 
         // Don't even inspect the intent, we only do one thing, and that's fetch content.
-        ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
+        ArrayList<ContentProviderOperation> cpo = new ArrayList<>();
 
         Uri dirUri = ItemsContract.Items.buildDirUri();
 
